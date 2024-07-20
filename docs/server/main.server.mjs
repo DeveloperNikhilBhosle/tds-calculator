f58c4d5a62af1ac08edb9be68b2aaccbfcc52dd3
Nikhil <nikhil.bhosle@atriina.com>
2024-07-20 13:17:47 +0530

    NIKHIL :: index page update

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-TEE5EY3H.mjs";import"./chunk-MJLUMB4L.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-UZUTEUDG.mjs";import"./chunk-CPPYNGZ2.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
